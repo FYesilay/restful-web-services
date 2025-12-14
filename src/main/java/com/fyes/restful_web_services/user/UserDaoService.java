cf250a8be16f59d3d29ec1af5b5dc2dab5158912
FYesilay <farukyesilay@outlook.de>
2025-12-14 19:24:20 +0100

    delete a User Resource

--- src/main/java/com/fyes/restful_web_services/user/UserDaoService.java
+++ src/main/java/com/fyes/restful_web_services/user/UserDaoService.java
@@ -25,9 +25,12 @@ public class UserDaoService {
 		return users;
 	}
 	public User findOne(int id) {
-	
-
 		Predicate<? super User>predicate=user->user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+	
+	public void deleteById(int id) {
+		Predicate<? super User>predicate=user->user.getId().equals(id);
+		users.removeIf(predicate);
 	}
 }
